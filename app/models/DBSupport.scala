8cf435ea54d22732626c1112f9b54936e4d76c83
竹内俊貴 <cs11054@s.inf.shizuoka.ac.jp>
2014-10-21 20:24:50 +0900

    heroku

--- app/models/DBSupport.scala
+++ app/models/DBSupport.scala
@@ -6,12 +6,17 @@ import play.Play
 
 trait DBSupport {
 
+  val DB_PATH = Play.application().configuration().getString("db.default.url")
+  val DRIVER = if (DB_PATH.contains("localhost")) "org.h2.Driver" else "org.postgresql.Driver"
+  val USER = if (DB_PATH.contains("localhost")) "sa" else "zcgmsoybywazlx"
+  val PASS = if (DB_PATH.contains("localhost")) "" else "HQt5HBNiYxgDE4LdQ337Ry_urb"
+
   // ソートの逆順用
   def Desc[T: Ordering] = implicitly[Ordering[T]].reverse
 
   // DBへの接続を補助
   def connectDB[T](f: => T): T = {
-    Database.forURL(Play.application().configuration().getString("db.default.url"), driver = "org.h2.Driver", user = "sa") withSession {
+    Database.forURL(DB_PATH, driver = DRIVER, user = USER, password = PASS) withSession {
       f
     }
   }
