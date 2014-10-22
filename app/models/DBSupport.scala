f9480cf147a3b51d6b2873222cdf23a1a7be3b9d
竹内俊貴 <cs11054@s.inf.shizuoka.ac.jp>
2014-10-22 14:46:11 +0900

    make it better

--- app/models/DBSupport.scala
+++ app/models/DBSupport.scala
@@ -6,20 +6,21 @@ import Database.threadLocalSession
 
 trait DBSupport {
 
+  val Heroku = true // or false
+
+  val DB_PATH = if (!Heroku) "jdbc:h2:tcp://localhost:9093/db"
+  else "postgres://zcgmsoybywazlx:HQt5HBNiYxgDE4LdQ337Ry_urb@ec2-54-83-204-78.compute-1.amazonaws.com:5432/dcn5isfl4oitng"
+
+  val DRIVER = if (DB_PATH.contains("localhost")) "org.h2.Driver" else "org.postgresql.Driver"
+  val USER = if (DB_PATH.contains("localhost")) "sa" else "zcgmsoybywazlx"
+  val PASS = if (DB_PATH.contains("localhost")) null else "HQt5HBNiYxgDE4LdQ337Ry_urb"
+
   // ソートの逆順用
   def Desc[T: Ordering] = implicitly[Ordering[T]].reverse
 
-  val heroku = true
-
-  val DB_URL = if (!heroku) "jdbc:h2:tcp://localhost:9093/db"
-  else "postgres://zcgmsoybywazlx:HQt5HBNiYxgDE4LdQ337Ry_urb@ec2-54-83-204-78.compute-1.amazonaws.com:5432/dcn5isfl4oitng"
-  val DRIVER = if (!heroku) "org.h2.Driver" else "org.postgresql.Driver"
-  val USER = if (!heroku) "sa" else " 	zcgmsoybywazlx"
-  val PASS = if (!heroku) "" else "HQt5HBNiYxgDE4LdQ337Ry_urb"
-
   // DBへの接続を補助
   def connectDB[T](f: => T): T = {
-    Database.forURL(DB_URL, driver = DRIVER, user = USER, password = PASS) withSession {
+    Database.forURL(DB_PATH, driver = DRIVER, user = USER, password = PASS) withSession {
       f
     }
   }
